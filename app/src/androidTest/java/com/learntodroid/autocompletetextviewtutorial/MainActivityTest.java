4ec36238c8d50834f4c73226ea0744ddb98d7293
learntodroid <56079350+learntodroid@users.noreply.github.com>
2020-04-12 10:47:34 +1000

    Update MainActivityTest.java

--- app/src/androidTest/java/com/learntodroid/autocompletetextviewtutorial/MainActivityTest.java
+++ app/src/androidTest/java/com/learntodroid/autocompletetextviewtutorial/MainActivityTest.java
@@ -1,6 +1,5 @@
 package com.learntodroid.autocompletetextviewtutorial;
 
-import androidx.test.espresso.matcher.RootMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
@@ -10,14 +9,15 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.isPlatformPopup;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.equalTo;
+import static java.lang.Thread.sleep;
+import static org.hamcrest.Matchers.containsString;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
@@ -26,8 +26,13 @@ public class MainActivityTest {
 
     @Test
     public void autoCompleteTextViewTest() {
-        onView(withId(R.id.activity_main_autoCompleteTextView)).perform(typeText("Melbou"));
-        onData(equalTo("Melbourne")).inRoot(RootMatchers.isPlatformPopup()).perform(click());
-        onView(withId(R.id.activity_main_selected_place_name)).check(matches(withText("Melbourne")));
+        onView(withId(R.id.activity_main_autoCompleteTextView)).perform(typeText("Melbour"));
+        onView(withText(containsString("Melbourne Airport (MEL)"))).inRoot(isPlatformPopup()).perform(click());
+        try {
+            sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(withId(R.id.activity_main_selected_place_name)).check(matches(withText("Selected Place: Melbourne Airport")));
     }
 }
